3e27ceef9d74709e0fbf5b7969d0093f2acee1fe
Ayal Ciobotaru <ayal.ciobo@gmail.com>
2015-12-07 14:43:03 -0500

    First few lines

---
+++ app_analytics.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas_datareader.io as web
+import keen
+
+project_id = open("project_id.txt", 'r')
+mastery_key = open("master_key.txt", 'r')
+write_key = open("write_key.txt", 'r')
+read_key = open("read_key.txt", 'r')
+
