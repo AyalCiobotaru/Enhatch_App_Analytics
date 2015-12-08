eb35f21024b01f92bcaaff9f659b17e733938fcc
Ayal Ciobotaru <ayal.ciobo@gmail.com>
2015-12-08 09:51:55 -0500

    deleted extra space in the API Keys, pull works

--- app_analytics.py
+++ app_analytics.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import pandas_datareader.io as web
 import keen
+from keen.client import KeenClient
 
-project_id = open("project_id.txt", 'r')
-mastery_key = open("master_key.txt", 'r')
-write_key = open("write_key.txt", 'r')
-read_key = open("read_key.txt", 'r')
+client = KeenClient(
+    project_id=open("project_id.txt", 'r').read(),
+    master_key=open("master_key.txt", 'r').read(),
+    write_key=open("write_key.txt", 'r').read(),
+    read_key=open("read_key.txt", 'r').read()
+)
 
+temp_df = pd.DataFrame()
+app_data = client.count_unique('Page', 'user.pk',
+                               timeframe='last_7_days',
+                               timezone=5,
+                               interval='daily')
+
+print(temp_df)
