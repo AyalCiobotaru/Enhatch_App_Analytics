d7dadf4bc5eb5cee5545e8ae5c945af18cff27fe
Ayal Ciobotaru <ayal.ciobo@gmail.com>
2015-12-08 13:02:55 -0500

    Daily pull added

--- app_analytics.py
+++ app_analytics.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pandas_datareader.io as web
 import keen
 from keen.client import KeenClient
+import datetime as dt
 
 client = KeenClient(
     project_id=open("project_id.txt", 'r').read(),
@@ -10,10 +11,53 @@ client = KeenClient(
     read_key=open("read_key.txt", 'r').read()
 )
 
-temp_df = pd.DataFrame()
-app_data = client.count_unique('Page', 'user.pk',
-                               timeframe='last_7_days',
-                               timezone=5,
-                               interval='daily')
 
-print(temp_df)
+def end_day():
+    day = int(input("What day would you like the query to end?"))
+    return day
+
+
+def end_month():
+    month = int(input("What month would you like the query to end?"))
+    return month
+
+
+def end_year():
+    year = int(input("What year would you like the query to end?"))
+    return year
+
+
+def daily_query_start(day, month, year):
+    start_day = dt.datetime(year, month, day) - dt.timedelta(30)
+    query = {'end': str(dt.datetime(year, month, day).date()), 'start': str(start_day.date())}
+    return query
+
+
+
+def extract_date(raw_data):
+    x = 0
+    while x < len(raw_data):
+
+        get_first_column = raw_data[x]
+        get_first_time = get_first_column['timeframe']
+        get_first_column['Date'] = get_first_column.pop('timeframe')
+        get_first_column['Date'] = dt.datetime.strptime(get_first_time['end'],'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%m/%d/%y')
+        x += 1
+
+
+def app_data_daily(month, day, year):
+    temp_df = pd.DataFrame()
+    app_data = client.count_unique('Page', 'user.pk',
+                                   timeframe=daily_query_start(day, month, year),
+                                   timezone=5,
+                                   interval='daily')
+    extract_date(app_data)
+    if temp_df.empty:
+        temp_df = pd.DataFrame(app_data)
+    else:
+        temp_df = temp_df.join(app_data)
+
+    temp_df.set_index('Date',inplace=True)
+    print(temp_df)
+
+app_data_daily(end_month(), end_day(), end_year())
